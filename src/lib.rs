1f067861a1cff60c63db45230a486275de3884a6
Ethan Brierley <ethanboxx@gmail.com>
2018-09-17 15:31:42 +0100

    copy docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
 use std::io;
-
+/// ### Close with enter.
+/// This waits for the user to press enter then ends to program (closing the window).
+/// Add
+/// ```rust
+/// extern crate dont_disappear;
+/// ```
+/// to the top of your file and
+/// ```rust
+/// dont_disappear::enter_to_continue();
+/// ```
+/// to where your program ends
 pub fn enter_to_continue() {
     let mut input = String::new();
     println!("Press enter to close.");
