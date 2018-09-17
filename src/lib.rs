dac5e2aba4a11c058de9251014dc44b3e3b2150a
Ethan Brierley <ethanboxx@gmail.com>
2018-09-17 15:56:39 +0100

    new custom_enter_to_continue() command

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,33 @@
 use std::io;
 /// ### Close with enter.
-/// This waits for the user to press enter then ends to program (closing the window).
+/// Prompts user with message `"Press enter to close."`, waits for the user to press enter then ends to program (closing the window).
 /// Add
 /// ```rust
 /// extern crate dont_disappear;
 /// ```
-/// to the top of your file and
+/// to the top of your file
+/// and
 /// ```rust
 /// dont_disappear::enter_to_continue();
 /// ```
 /// to where your program ends
 pub fn enter_to_continue() {
+    custom_enter_to_continue("Press enter to close.".to_string());
+}
+/// ### Close with enter.
+/// Prompts user with a custom message, waits for the user to press enter then ends to program (closing the window).
+/// Add
+/// ```rust
+/// extern crate dont_disappear;
+/// ```
+/// to the top of your file
+/// and
+/// ```rust
+/// dont_disappear::custom_enter_to_continue("Your custom message.".to_string());
+/// ```
+/// to where your program ends
+pub fn custom_enter_to_continue(msg: String) {
     let mut input = String::new();
-    println!("Press enter to close.");
+    println!("{}", msg);
     io::stdin().read_line(&mut input).unwrap();
 }
