618ea88b1d4a7ddf7211302c8d3f5a60d1bfd903
Ethan Brierley <ethanboxx@gmail.com>
2018-09-17 21:41:22 +0100

    ignore

--- examples/enter_to_continue.rs
+++ examples/enter_to_continue.rs
@@ -1,6 +1,7 @@
 //cargo run --example enter_to_continue
 extern crate dont_disappear;
 
+#[ignore]
 fn main() {
     println!("Here is some data");
     dont_disappear::enter_to_continue::default();
