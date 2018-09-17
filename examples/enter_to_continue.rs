e4b577c28f9f0b80388199ab43456d970af4b25f
Ethan Brierley <ethanboxx@gmail.com>
2018-09-17 22:09:13 +0100

    fix never ending test issue

--- examples/enter_to_continue.rs
+++ examples/enter_to_continue.rs
@@ -1,7 +1,6 @@
 //cargo run --example enter_to_continue
 extern crate dont_disappear;
 
-#[ignore]
 fn main() {
     println!("Here is some data");
     dont_disappear::enter_to_continue::default();
